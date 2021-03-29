f970e71cd3d7e30170a009370cc20e703c5764a9
SID479 <73699216+SID479@users.noreply.github.com>
2021-03-29 17:58:04 +0530

    Add files via upload

---
+++ water tank problem.py
@@ -0,0 +1,17 @@
+h=int(input("enter the height"))
+r=int(input("enter the radius"))
+f=int(input("enter the rate of flow"))
+t=int(input("enter the time"))
+volume=3.14*r**2
+vrate=f*t
+if vrate > volume:
+        print("over flow condition")
+        print("water overflowed",vrate - volume)
+elif vrate == volume :
+        print("tank full")
+else:
+        print("underflow condition")
+        ht=vrate/(3.14*r**2)
+        hr=h-ht
+        print(f"filled height(ht)\n remaing height(hr)")
+        
